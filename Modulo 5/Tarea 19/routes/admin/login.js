4144d6566a2e70c6a34e85bbc66c37e239ea350d
Nicolás <nicotomas181@gmail.com>
2021-11-22 15:10:29 -0300

    crud part 3

---
+++ Modulo 5/Tarea 19/routes/admin/login.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var usuariosmodels = require('./../../models/usuariosmodels');
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('admin/login', {
+        layout: 'admin/layout',
+    });
+   
+}); 
+
+    // para salir
+
+    router.get('/logout', function (req, res, next) {
+        req.session.destroy();
+        res.render('admin/login', {
+            layout: 'admin/layout'
+        });
+       
+    });
+
+    router.post('/', async (req, res, next) => {
+        try {
+            var user = req.body.usuario;
+            var password = req.body.password;
+
+            console.log(req.body);
+
+            var data = await usuariosmodels.getUserAndPassword(user, password);
+
+            if (data != undefined) {
+                req.session.id_usuario = data.id,
+                    req.session.nombre = data.usuario,
+                    res.redirect('/admin/novedades');
+            } else {
+                res.render('admin/login', {
+                    layout: 'admin/layout',
+                    error: true
+                });
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    });
+
+    module.exports = router;
---
+++ Modulo 5/Tarea 19/routes/index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var novedadesmodels = require('./../models/novedadesmodels');
+var cloudinary = require('cloudinary').v2;
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  var novedades = await novedadesmodels.getNovedades();
+novedades=novedades.splice(0,5); //seleccionamos los primeros 5
+novedades=novedades.map(novedad =>{
+  if (novedad.img_id) {
+    const imagen =cloudinary.url(novedad.img_id, {
+      width:460,
+      crop: 'fill'
+    });
+    return {
+      ...novedad,
+      imagen
+    }
+  } else {
+    return {
+      ...novedad,
+      imagen: '/images/imagenerror.jpg'
+    }
+  }
+});
+
+
+  res.render('index', {
+    novedades
+  });
+});
+
+
+
+router.post('/', async (req, res, next) => {
+
+  var email = req.body.email;
+  var contraseña = req.body.contraseña;
+  var direccion = req.body.direccion;
+  var nombreyapellido = req.body.nombreyapellido;
+  var direccion2 = req.body.direccion2;
+  var ciudad = req.body.ciudad;
+  var codigopostal = req.body.codigopostal;
+
+  // console.log(req.body);
+
+  var obj = {
+    to: 'nicotomas181@gmail.com',
+    subjetc: 'contacto desde la web',
+    html: nombre + " " + apellido + "se contacto a traves y quiere mas info a este correo: " + email +
+      ". <br> Ademas se hizo el siguiente comentario: " + mensaje + ". <br> su tel es: " + telefono
+  }
+
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+
+  })
+
+  var info = await transporter.sendMail(obj);
+
+  res.render('index', {
+    message: 'mensaje enviado correctamente'
+  });
+
+});
+
+module.exports = router;
