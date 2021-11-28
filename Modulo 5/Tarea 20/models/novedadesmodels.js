d3389911aedac631fced64766916a2c7981d7de4
Nicolás <nicotomas181@gmail.com>
2021-11-28 20:20:52 -0300

    agrego buscador

--- Modulo 5/Tarea 20/models/novedadesmodels.js
+++ Modulo 5/Tarea 20/models/novedadesmodels.js
@@ -10,38 +10,47 @@ async function getNovedades() {
 
 async function deleteNovedadesById(id) {
     var query = 'delete from novedades where id=?';
-    var rows = await pool.query (query, [id]);
+    var rows = await pool.query(query, [id]);
     return rows;
-    }
+}
 
-    async function insertNovedad(obj) {
-        try {
+async function insertNovedad(obj) {
+    try {
         var query = "insert into novedades set ?";
-        var rows = await pool.query (query, [obj])
+        var rows = await pool.query(query, [obj])
         return rows;
-        } catch(error) {
-            console.log (error);
-            throw error;
-        }
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
+}
 
-    // para modificar que me traiga solo 1 novedad por id
+// para modificar que me traiga solo 1 novedad por id
 
-    async function getNovedadById(id) {
-        var query= "select *from novedades where id=?";
-        var rows= await pool.query(query, [id]);
-        return rows [0];
-    }
+async function getNovedadById(id) {
+    var query = "select *from novedades where id=?";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
 // update
 
 async function modificarNovedadById(obj, id) {
     try {
-        var query= "update novedades set ? where id=?";
-        var rows= await pool.query(query, [obj, id]);
+        var query = "update novedades set ? where id=?";
+        var rows = await pool.query(query, [obj, id]);
         return rows;
     } catch (error) {
         throw error;
     }
 }
 
-module.exports = {getNovedades, deleteNovedadesById, insertNovedad, getNovedadById, modificarNovedadById}
+async function buscarNovedades(busqueda) {
+    var query = "select * from novedades where titulo like ? OR subtitulo like ? OR cuerpo like ?";
+    var rows = await pool.query(query, ['%' + busqueda + '%', '%' + busqueda + '%', '%' + busqueda + '%']);
+    return rows;
+}
+
+module.exports = {
+    getNovedades, deleteNovedadesById, insertNovedad, getNovedadById, modificarNovedadById,
+    buscarNovedades
+}
--- Modulo 5/Tarea 20/routes/admin/novedades.js
+++ Modulo 5/Tarea 20/routes/admin/novedades.js
@@ -10,7 +10,15 @@ const destroy = util.promisify(cloudinary.uploader.destroy);
 /* GET home page. */
 router.get('/', async function (req, res, next) {
 
-    var novedades = await novedadesmodels.getNovedades();
+    //var novedades = await novedadesmodels.getNovedades();
+
+    var novedades
+    if (req.query.q === undefined) {
+        novedades = await novedadesmodels.getNovedades();
+    } else {
+        novedades = await novedadesmodels.buscarNovedades(req.query.q);
+    }
+
     novedades = novedades.map(novedad => {
         if (novedad.img_id) {
             const imagen = cloudinary.image(novedad.img_id, {
@@ -34,7 +42,9 @@ router.get('/', async function (req, res, next) {
     res.render('admin/novedades', {
         layout: 'admin/layout',
         usuario: req.session.nombre,
-        novedades
+        novedades,
+        is_search: req.query.q !== undefined,
+        q: req.query.q
     });
 
 });
